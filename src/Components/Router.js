51f77271cf9bdbb53a23f361f441a2123ba4c5b2
MinChan Lee <shigatsu970704@gmail.com>
2020-11-25 18:45:05 +0900

    02.React Router Part Two

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/tv" exact component={TV} />
-    <Route path="/search" exact component={Search} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
