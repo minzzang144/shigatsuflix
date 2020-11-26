abbe8f0c71a0cf7648fbacc2b168c34665916fd1
MinChan Lee <shigatsu970704@gmail.com>
2020-11-26 12:20:41 +0900

    06.GlobalStyles and Header

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1rem;
+    background-color: rgba(20, 20, 20, 1);
+    color: white;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default globalStyles;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 15px;
+  color: white;
+  background-color: rgba(20, 20, 20, 0.8);
+  font-size: 12px;
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: blue;
-  }
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  width: 50px;
+  text-align: center;
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default () => (
   <Header>
