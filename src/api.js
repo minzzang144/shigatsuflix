f70e2e077e665a24cfbdaad6e3a1f9cc5c9b2b5e
MinChan Lee <shigatsu970704@gmail.com>
2020-11-27 10:34:22 +0900

    09.API Verbs part One

--- src/api.js
+++ src/api.js
@@ -10,4 +10,14 @@ const api = axios.create({
 
 api.get("tv/popular");
 
-export default api;
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+};
