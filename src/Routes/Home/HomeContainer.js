dfaf457f5c2e7a5bcd9492dfbf8a0cc26f3cc326
MinChan Lee <shigatsu970704@gmail.com>
2020-11-27 14:33:02 +0900

    14.TV Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -30,7 +30,7 @@ export default class extends React.Component {
       });
     } catch {
       this.setState({
-        error: "Can't find movies information.",
+        error: "Can't find movie information.",
       });
     } finally {
       this.setState({
--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { tvApi } from "api";
 import TVPresenter from "./TVPresenter";
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -11,8 +12,36 @@ export default class extends React.Component {
     error: null,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular },
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({
+        topRated,
+        popular,
+        airingToday,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find TV information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
+    console.log(this.state);
     return (
       <TVPresenter
         topRated={topRated}
