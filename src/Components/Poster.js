61fd9e3b2f08ba3583348e9603c1212881f350f3
MinChan Lee <shigatsu970704@gmail.com>
2020-11-30 10:43:20 +0900

    23.Poster Component part One

---
+++ src/Components/Poster.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const ImageContainer = styled.div``;
+
+const Image = styled.div``;
+
+const Rating = styled.span``;
+
+const Title = styled.span``;
+
+const Year = styled.span``;
+
+const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
+  <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
+    <Container>
+      <ImageContainer>
+        <Image bgUrl={imageUrl} />
+        <Rating>
+          <span role="img" aria-label="rating">
+            ⭐
+          </span>
+          {rating}/10
+        </Rating>
+        <Title>{title}</Title>
+        <Year>{year}</Year>
+      </ImageContainer>
+    </Container>
+  </Link>
+);
+
+Poster.propTypes = {
+  id: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  year: PropTypes.string,
+  isMovie: PropTypes.bool,
+};
+
+export default Poster;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,21 +18,21 @@ const HomePresenter = ({ nowPlaying, upComing, popular, loading, error }) =>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map((movie) => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular">
           {popular.map((movie) => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
       {upComing && upComing.length > 0 && (
         <Section title="Up Coming">
           {upComing.map((movie) => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster />
           ))}
         </Section>
       )}
