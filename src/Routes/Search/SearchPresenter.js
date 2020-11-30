79795c954c90c1abe2e29713f1dfae4152d620f6
MinChan Lee <shigatsu970704@gmail.com>
2020-11-30 10:09:55 +0900

    22.Message Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import Loader from "Components/Loader";
 import Section from "Components/Section";
+import Loader from "Components/Loader";
+import Message from "Components/Message";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -18,9 +19,6 @@ const Input = styled.input`
   all: unset;
   width: 100%;
   font-size: 1.5rem;
-  ::placeholder {
-    color: white;
-  }
 `;
 
 const SearchPresenter = ({
@@ -58,6 +56,13 @@ const SearchPresenter = ({
             ))}
           </Section>
         )}
+        {error && <Message text={error} color="#e74c3c" />}
+        {movieResults &&
+          tvResults &&
+          movieResults.length === 0 &&
+          tvResults.length === 0 && (
+            <Message text="Nothing Found" color="#95a5a6" />
+          )}
       </>
     )}
   </Container>
