e47bd32091c5b8cebb606334fa4e80937b07665b
MinChan Lee <shigatsu970704@gmail.com>
2020-12-02 12:34:32 +0900

    31.Card Detail

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,29 +6,56 @@ const ItemList = styled.div`
   display: flex;
   width: 100%;
   height: 100vh;
-  perspective: 660px;
+  perspective: 55vw;
   overflow-x: scroll;
   overflow-y: hidden;
   white-space: nowrap;
 `;
 
+const Title = styled.h3`
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+
+const Overview = styled.p``;
+
 const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding: 100px;
   min-width: 100%;
+  width: 100%;
   height: 100%;
   background-image: url(${(props) => props.bgImage});
   background-position: center center;
   background-size: cover;
-  &:nth-child(even) {
-    transform: rotateY(35deg);
-  }
+  text-align: right;
+  white-space: normal;
   &:nth-child(odd) {
-    transform: rotateY(-35deg);
+    transform: rotateY(-40deg) translateX(-180px);
+    ${Title} {
+      margin: 150px 0px 10px 0px;
+    }
+    ${Overview} {
+      margin-left: 50%;
+      opacity: 0.8;
+      line-height: 1.5;
+    }
+  }
+  &:nth-child(even) {
+    transform: rotateY(40deg) translateX(-180px);
+    text-align: left;
+    ${Title} {
+      margin: 200px 0px 10px 0px;
+    }
+    ${Overview} {
+      width: 50%;
+      opacity: 0.8;
+      line-height: 1.5;
+    }
   }
 `;
-
-const Title = styled.span``;
-
-const Overview = styled.p``;
 
 const Card = ({ nowPlaying }) => (
   <ItemList>
